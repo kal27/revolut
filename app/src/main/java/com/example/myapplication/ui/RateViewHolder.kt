6dec292a3870fbf5ec1d2123a45f78e811e74797
kal27 <kamlew@o2.pl>
2019-05-11 15:24:17 +0200

    Delete unnecessary code

--- app/src/main/java/com/example/myapplication/ui/RateViewHolder.kt
+++ app/src/main/java/com/example/myapplication/ui/RateViewHolder.kt
@@ -12,9 +12,7 @@ class RateViewHolder(inflater: LayoutInflater, parent: ViewGroup) :
 
     fun bind(rate: Pair<String, Double>) {
         itemView.rate_currency.text = rate.first
-        if (!itemView.rate_input.hasFocus()) {
-            itemView.rate_input.hint = String.format(itemView.resources.getString(R.string.rate_hint), rate.first)
-            itemView.rate_input.setText(rate.second.toString())
-        }
+        itemView.rate_input.hint = String.format(itemView.resources.getString(R.string.rate_hint), rate.first)
+        itemView.rate_input.setText(rate.second.toString())
     }
 }
